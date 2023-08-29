20f43f7a9e9cd838ab94448166f3a01d5e725c26
Tushar Singla <110544909+tushar412000@users.noreply.github.com>
2023-08-29 11:20:02 +0530

    Add files via upload

---
+++ sales.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Load the dataset
+data = pd.read_csv('advertising.csv')
+
+# Define the features and target variable
+X = data.drop('Sales', axis=1)
+y = data['Sales']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the Linear Regression model
+model = LinearRegression()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse:.2f}")
